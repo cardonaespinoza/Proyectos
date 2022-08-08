ec5fa7d2ad7e5b2d782f99570e88e76c9cc56f0d
Arturo Cardona <cardonaespinoza@gmail.com>
2022-08-07 18:32:54 -0600

    Conexion a Sqllite
    Conexion a Sqlite como base de datos local

---
+++ SqliteC/SqliteC/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace SqliteC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection ObjConnection = new SQLiteConnection("Data source=chinook.db;");
+            SQLiteCommand ObjCommand = new SQLiteCommand("Select * from customers", ObjConnection);
+
+            ObjCommand.CommandType = CommandType.Text;
+            SQLiteDataAdapter ObjdataAdapter = new SQLiteDataAdapter(ObjCommand);
+            DataSet dataset = new DataSet();
+            ObjdataAdapter.Fill(dataset, "customers");
+            dataGridView1.DataSource = dataset.Tables["customers"];
+        }
+
+
+
+
+    }
+}
