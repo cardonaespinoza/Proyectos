5b1f7aed5bee117cadc381ac628b8169c6b26a71
Octavio Arturo CE <60953225+cardonaespinoza@users.noreply.github.com>
2022-08-11 21:31:45 -0600

    Abstraccion y Encapsulacion
    Objetos

---
+++ PHP-POO/ClassUsuario.php
@@ -0,0 +1,22 @@
+<?php
+
+	class Usuario{
+
+		public $strNombre;
+		public $strEmail;
+		public $strTipo;
+		private $strClave;
+		protected $strFechaRegistro;
+		static $strEstado="Activo";
+
+		function __construct(string $nombre,string $email,string $tipo){
+			$this->strnombre=$nombre;
+			$this->strEmail=$email;
+			$this->strTipo=$tipo;
+			$this->strClave=rand();
+		}
+
+	}
+
+
+?>
